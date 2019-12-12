e3b00055d6cc152f07d57535e2af48bdf006e01b
Ryan Burbidge <ryan.burbidge@gmail.com>
2019-12-11 16:56:20 -0800

    Add initial version of Interceptor API

---
+++ src/test/java/com/sirnommington/interception/sample/LogBeginEndInterceptor.java
@@ -0,0 +1,16 @@
+package com.sirnommington.interception.sample;
+
+import com.sirnommington.interception.Interceptor;
+import com.sirnommington.interception.InterceptorContext;
+
+import java.util.function.Supplier;
+
+public class LogBeginEndInterceptor implements Interceptor {
+    @Override
+    public <T> T execute(InterceptorContext context, Supplier<T> operation) {
+        System.out.println("Begin operation " + context.operationName());
+        T result = context.proceed(operation);
+        System.out.println("End operation " + context.operationName());
+        return result;
+    }
+}
